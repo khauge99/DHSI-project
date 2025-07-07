a0c72d46a51d0be290ca43d1bc3ec8a3dc4d8d31
Kristin Hauge <kristin.carmella.hauge@gmail.com>
2025-07-07 12:16:35 -0400

    Adds updated files, still having trouble

---
+++ project_map.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const map = L.map('map').setView([44.6509, -63.5923], 3);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19, // Max zoom level for these tiles
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map); // Add the tile layer to our map instance
+
+$.getJSON("project_data.json", function(data) {
+    let placeArray = data.places;
+    let i = 0;
+    let markers = []; 
+    while (i < placeArray.length) {
+        markers[i] = new L.marker(placeArray[i].coordinates);
+        markers[i].addTo(map).bindPopup(placeArray[i].location);  
+        i++;
+    }     
+});
+
+
+
+
+
+
+//function onClick(e) {
+    //$("#myModal").modal("show");
+}
+
+);
