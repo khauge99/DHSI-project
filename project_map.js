f80b08423eadd7c0ff2745e6ccf6cdc80b8df86d
Kristin Hauge <kristin.carmella.hauge@gmail.com>
2025-07-07 21:49:03 -0400

    Mostly done except for some photos

--- project_map.js
+++ project_map.js
@@ -6,20 +6,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map); // Add the tile layer to our map instance
 
+
+//addEventListener for click?
+//Modal vs image in popup vs new html page
+
 $.getJSON("project_data.json", function(data) {
     let placeArray = data.places;
     let i = 0;
     let markers = []; 
     while (i < placeArray.length) {
         markers[i] = new L.marker(placeArray[i].coordinates);
-        markers[i].addTo(map).bindPopup(placeArray[i].location);  
+        markers[i].addTo(map).bindPopup(placeArray[i].location + placeArray[i].image);  
         i++;
     }     
 });
-
-
-
-
 
 
 //function onClick(e) {
